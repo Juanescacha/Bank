5754d3dc3b3f55b430aaeee888acf5f99890210e
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-04-06 10:32:33 -0500

    basic component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styles from "./styles"
+import {
+	Navbar,
+	Billing,
+	CardDeal,
+	Business,
+	Clients,
+	CTA,
+	Stats,
+	Footer,
+	Testimonials,
+	Hero,
+} from "./components"
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div className="bg-primary w-full overflow-hidden text-white">
+			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth}`}>
+					<Navbar />
+				</div>
+			</div>
+
+			<div className={`${styles.flexStart} bg-primary`}>
+				<div className={`${styles.boxWidth}`}>
+					<Hero />
+				</div>
+			</div>
+
+			<div
+				className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
+				<div className={`${styles.boxWidth}`}>
+					<Stats />
+					<Business />
+					<Billing />
+					<CardDeal />
+					<Testimonials />
+					<Clients />
+					<CTA />
+					<Footer />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
