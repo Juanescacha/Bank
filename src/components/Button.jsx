5477d0df7ecb98e0dca81217d3e1ccfeec86ab17
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-04-08 12:45:25 -0500

    Card Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Button = ({ style }) => (
 	<button
 		type="button"
-		className={`px-6 py-4 text-lg font-medium rounded-lg outline-none font-poppins bg-blue-gradient text-primary ${style}`}>
+		className={`px-6 py-4 text-lg font-medium rounded-[10px] outline-none font-poppins bg-blue-gradient text-primary ${style}`}>
 		Get Started
 	</button>
 )
