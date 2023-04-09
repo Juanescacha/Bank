16448da7d3e96daf2e4baea48cb3b5912d3147e4
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-04-08 21:53:14 -0500

    almost completed testimonials

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,33 @@
-import React from "react"
+import { quotes } from "../assets"
+import styles from "../styles"
 
-const FeedbackCard = () => {
-	return <div>FeedbackCard</div>
-}
+const FeedbackCard = ({ id, name, content, title, img, index }) => (
+	<div
+		className={`basis-full feedback-card rounded-[20px] py-10 flex-col gap-10 flex justify-center items-start px-12`}>
+		<img
+			src={quotes}
+			alt="quotes"
+			className="object-contain w-[42px] h-[27px]"
+		/>
+		<p className="text-lg font-normal leading-[32px] text-white font-poppins">
+			{content}
+		</p>
+		<div className="flex items-center w-full gap-5">
+			<img
+				src={img}
+				alt="img"
+				className="object-contain w-[48px] h-[48px]"
+			/>
+			<div className="flex flex-col w-full">
+				<h4 className="text-xl font-normal text-white font-poppins">
+					{name}
+				</h4>
+				<p className="text-base font-normal font-poppins text-dimWhite">
+					{title}
+				</p>
+			</div>
+		</div>
+	</div>
+)
 
 export default FeedbackCard
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,38 @@
-import React from "react"
+import styles, { layout } from "../styles"
+import { quotes } from "../assets"
+import { feedback } from "../constants"
+import FeedbackCard from "./FeedbackCard"
 
-const Testimonials = () => {
-	return <div>Testimonials</div>
-}
+const Testimonials = () => (
+	<section>
+		<div className={`${layout.section}`}>
+			<div className={`${layout.sectionInfo}`}>
+				<h1 className={`${styles.heading2}`}>
+					What people are
+					<br className="hidden sm:block" /> saying about us.
+				</h1>
+			</div>
+
+			<div
+				className={`flex flex-1 justify-start items-center md:ml-10 ml-0 md:mt-0 mt-10 relative`}>
+				<p
+					className={`${styles.paragraph}  max-w-[450px] text-left mt-5`}>
+					Everything you need to accept card payments and grow your
+					business anywhere on the planet.
+				</p>
+			</div>
+		</div>
+		<div
+			className={`${layout.section} justify-center gap-10 relative z-[1] items-stretch`}>
+			{feedback.map((item, index) => (
+				<FeedbackCard
+					key={item.id}
+					index={index}
+					{...item}
+				/>
+			))}
+		</div>
+	</section>
+)
 
 export default Testimonials
