26b24409ef2c80c7ce38b68f890b5273a51be7cb
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-07-29 00:07:45 -0500

    navbar relative to fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,15 @@ import {
 const App = () => {
 	return (
 		<div className="w-full overflow-hidden bg-primary">
-			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
-				<div className={`${styles.boxWidth}`}>
+			<div className={`${styles.paddingX} ${styles.flexCenter} relative`}>
+				<div
+					className={`w-full z-50 bg-primary/0 backdrop-blur-xl fixed top-0 sm:px-32 px-12`}>
+					{/* className={`w-full z-50 bg-primary/0 backdrop-blur-xl top-0 ${styles.paddingX}`}> */}
 					<Navbar />
 				</div>
 			</div>
 
-			<div className={`${styles.flexStart} bg-primary`}>
+			<div className={`${styles.flexStart} mt-10 bg-primary`}>
 				<div className={`${styles.boxWidth}`}>
 					<Hero />
 				</div>
